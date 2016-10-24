b8d5ccfcdefc6471f95d050c83fa6376445ffc4c
Bryan <bolivarbryan@gmail.com>
2016-10-24 09:22:22 -0500

    first commit

---
+++ RappStore/API/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  RappStore
+//
+//  Created by Bryan A Bolivar M on 10/24/16.
+//  Copyright © 2016 Bolivar. All rights reserved.
+//
+
+import Foundation
+
+let kApiEnvironment = "https://itunes.apple.com/"
+let kApiUrl = "us/rss/topfreeapplications/limit=20/json"
---
+++ RappStore/Model/App.swift
@@ -0,0 +1,13 @@
+//
+//  App.swift
+//  RappStore
+//
+//  Created by Bryan A Bolivar M on 10/24/16.
+//  Copyright © 2016 Bolivar. All rights reserved.
+//
+
+import UIKit
+
+class App: NSObject {
+
+}
